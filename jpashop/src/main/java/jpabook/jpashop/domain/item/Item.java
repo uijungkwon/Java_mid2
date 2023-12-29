112dcb452cecc5be32a82efbc4d3102ae0ad4166
uijung kwon <kuj08036410@gmail.com>
2023-12-29 18:08:37 +0900

    2023.12.28 - 엔터티 클래스 개발 완료

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //한 테이블에 모두 넣기
+@DiscriminatorColumn(name = "dtype") //저장할 때 구분하기 위한 용도(북, 무비, 앨범)
+@Getter @Setter
+public abstract class Item {
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}
